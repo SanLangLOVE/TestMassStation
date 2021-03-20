361494c09bfd5c3e8fc25987926935dd2f4721a8
sanlang <179874957@qq.com>
2021-03-20 10:53:46 +0800

    A file to test how to use git

---
+++ src/hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author sanlang
+ *
+ */
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello sanlang");
+        System.out.println("hello world");
+    }
+}
