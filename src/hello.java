1ff367ad0a078584b4c2f43b822a90046a0cdfe3
sanlang <179874957@qq.com>
2021-03-20 10:54:25 +0800

    A file to test how to use git

--- src/hello.java
+++ src/hello.java
@@ -6,5 +6,12 @@ class Hello {
     public static void main(String[] args) {
         System.out.println("hello sanlang");
         System.out.println("hello world");
+        System.out.println("hello sanlang");
+        System.out.println("hello world");
+        System.out.println("hello sanlang");
+        System.out.println("hello world");
+        System.out.println("hello sanlang");
+        System.out.println("hello world");
+
     }
 }
